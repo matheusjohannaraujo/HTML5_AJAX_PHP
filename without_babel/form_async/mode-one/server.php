59f797de1677d903dcef7471153332b80d271798
Matheus Johann Araújo <47996428+matheusjohannaraujo@users.noreply.github.com>
2020-01-11 14:50:01 -0300

    Create server.php

---
+++ without_babel/form_async/mode-one/server.php
@@ -0,0 +1,50 @@
+<?php
+
+// Brasil\Pernambuco
+// Developer: Matheus Johann Araujo
+// Date: 11-01-2020
+// GitHub: https://github.com/matheusjohannaraujo/lib_ajax_and_form_async
+
+if(!empty($_GET)){
+	echo "GET: ";
+	var_export($_GET);
+	echo "\r\n";
+}
+
+if(!empty($_POST)){
+	echo "POST: ";
+	var_export($_POST);
+	echo "\r\n";
+}
+
+if(!empty($_FILES)){
+	echo "FILES: ";
+	var_export($_FILES);
+	echo "\r\n";
+}
+
+if($_SERVER['REQUEST_METHOD'] == 'PUT') {
+	echo "PUT: ";
+	$_PUT = "";
+	$putData = fopen("php://input", "r");
+	while($data = fread($putData, 1024)){
+		$_PUT .= $data;
+	}
+	fclose($putData);
+	var_export($_PUT);
+	echo "\r\n";
+}
+
+if($_SERVER['REQUEST_METHOD'] == 'DELETE') {
+	echo "DELETE: ";
+	$_DELETE = $_SERVER['PATH_INFO'];
+	var_export($_DELETE);
+	echo "\r\n";
+}
+
+if($_SERVER['REQUEST_METHOD'] == 'PATCH') {
+	echo "PATCH: ";
+	$_PATCH = (array) json_decode(file_get_contents('php://input'));
+	var_export($_PATCH);
+	echo "\r\n";
+}
